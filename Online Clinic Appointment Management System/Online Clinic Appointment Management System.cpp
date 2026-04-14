330ac2f57c1c7250ba9ef94d4026efa6b6799248
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2026-04-14 02:15:43 +0200

    Added the book appointment function

--- Online Clinic Appointment Management System/Online Clinic Appointment Management System.cpp
+++ Online Clinic Appointment Management System/Online Clinic Appointment Management System.cpp
@@ -15,7 +15,7 @@ adminsCount = 1;
 
 // Functions Declarations
 void loadData();
-void chooseAdminOrPatient();
+void chooseAdminOrPatient(bool isRedirect);
 void terminateTheProgram();
 
 void viewDoctors(bool isRedirect);
@@ -29,6 +29,7 @@ void viewAllAppointments();
 void markAppointmentAsCompleted();
 void adminLogout();
 
+void patientLoginOrSignUp();
 void patientSignUp();
 void patientLogin();
 void patientDashboard();
@@ -88,10 +89,8 @@ Admin admins[2];
 
 // Main function
 int main() {
-	srand(time(NULL));
 	loadData();
-	chooseAdminOrPatient();
-	viewDoctors(false);
+	chooseAdminOrPatient(false);
 }
 
 
@@ -154,7 +153,45 @@ void loadData() {
 	data.close();
 
 };
-void chooseAdminOrPatient() {};
+void chooseAdminOrPatient(bool isRedirect) {
+
+	int choice = 0;
+
+	if (!isRedirect) {
+		cout << "******************************************************" << endl;
+		cout << "*           Welcome To The Online Clinic App         *" << endl;
+		cout << "******************************************************" << endl;
+	}
+
+	do {
+
+		cout << "\n";
+		cout << "------------------------------------------" << endl;
+		cout << "Choose an option to continue: " << endl;
+		cout << "1. Admin" << endl;
+		cout << "2. Patient" << endl;
+		cout << "3. Exit" << endl;
+		cout << "Your choice: ";
+		cin >> choice;
+
+		switch (choice) {
+			case 1:
+				adminLogin();
+				break;
+			case 2:
+				patientLoginOrSignUp();
+				break;
+			case 3:
+				terminateTheProgram();
+				break;
+			default:
+				cout << "Invalid choice. Try again." << endl;
+		}
+
+	} while (choice != 1 && choice != 2 && choice != 3);
+
+	
+};
 void terminateTheProgram() {
 
 	// Opening the file
@@ -239,10 +276,251 @@ void viewAllAppointments() {};
 void markAppointmentAsCompleted() {};
 void adminLogout() {};
 
-void patientSignUp() {};
+void patientLoginOrSignUp() {
+
+	int choice = 0;
+
+	do {
+
+		cout << "\n";
+		cout << "------------------------------------------" << endl;
+		cout << "Do you want to log in or sign up?: " << endl;
+		cout << "1. Login" << endl;
+		cout << "2. Signup" << endl;
+		cout << "3. Previous page" << endl;
+		cout << "Your choice: ";
+		cin >> choice;
+
+		switch (choice) {
+		case 1:
+			patientLogin();
+			break;
+		case 2:
+			patientSignUp();
+			break;
+		case 3:
+			chooseAdminOrPatient(true);
+			break;
+		default:
+			cout << "Invalid choice. Try again." << endl;
+		}
+
+	} while (choice != 1 && choice != 2 && choice != 3);
+
+};
+void patientSignUp() {
+
+	cout << "\n";
+	cout << "------------------------------------------" << endl;
+	cout << "\tSign Up" << endl;
+
+	// Patient id
+	int patientId = patientsCount + 1;
+
+	// Name
+	string name = "";
+	do {
+
+		string input;
+		cout << "Enter your name: ";
+		cin.ignore();
+		getline(cin, input);
+		
+		if (input == "") {
+			cout << "Invalid input. Try again.";
+		}
+		else {
+			name = input;
+		}
+
+	} while (name == "");
+
+	// Password
+	string password = "";
+	do {
+
+		string input;
+		cout << "Enter your password: ";
+		cin.ignore();
+		getline(cin, input);
+
+		if (input == "") {
+			cout << "Invalid Input. Try again." << endl;
+		}
+		else if (input.size() < 6) {
+			cout << "Password must be at least 6 characters" << endl;
+		}
+		else {
+			password = input;
+		}
+
+	} while (password == "");
+
+	// Phone number
+	string phoneNumber = "";
+	do {
+
+		string input;
+		cout << "Enter your phone number: ";
+		cin.ignore();
+		getline(cin, input);
+
+		if (input == "") {
+			cout << "Invalid Input. Try again." << endl;
+		}
+		else if (input.size() != 11) {
+			cout << "Phone number must contain 11 digits" << endl;
+		}
+		else {
+			phoneNumber = input;
+		}
+
+	} while (phoneNumber == "");
+
+	// Age
+	int age = -1;
+	do {
+
+		int input;
+		cout << "Enter your age: ";
+		cin >> input;
+
+		if (input <= 0 || input > 120) {
+			cout << "Invalid Input. Try again." << endl;
+		}
+		else if (input <= 0 || input > 120) {
+			cout << "Invalid Age. choose from 1 to 120" << endl;
+		}
+		else {
+			phoneNumber = input;
+		}
+
+	} while (age < 0);
+
+};
 void patientLogin() {};
 void patientDashboard() {};
-void bookAppointment() {};
+void bookAppointment() {
+
+	viewDoctors(false); // عرض الدكاترة
+
+	if (doctorsCount == 0) {
+		cout << "No doctors available.\n";
+		return;
+	}
+
+	int doctorID;
+	cout << "Enter Doctor ID: ";
+	cin >> doctorID;
+
+	// البحث عن الدكتور
+	int doctorIndex = -1;
+	for (int i = 0; i < doctorsCount; i++) {
+		if (doctors[i].doctorID == doctorID) {
+			doctorIndex = i;
+			break;
+		}
+	}
+
+	if (doctorIndex == -1) {
+		cout << "Invalid Doctor ID\n";
+		return;
+	}
+
+	// التأكد من وجود Slots
+	if (doctors[doctorIndex].availableSlots <= 0) {
+		cout << "No available slots for this doctor.\n";
+		return;
+	}
+
+	// أخذ التاريخ والوقت
+	Date date;
+	Time time;
+
+	cout << "Enter Day: ";
+	cin >> date.day;
+	cout << "Enter Month: ";
+	cin >> date.month;
+	cout << "Enter Year: ";
+	cin >> date.year;
+
+	cout << "Enter Hours (1–12): ";
+	cin >> time.hours;
+	cout << "Enter Minutes (0–59): ";
+	cin >> time.minutes;
+	cout << "Enter Time Period (AM/PM): ";
+	cin >> time.timePeriod;
+
+	if (time.timePeriod != "AM" && time.timePeriod != "PM") {
+		cout << "Invalid time period.\n";
+		return;
+	}
+
+	// تضارب مواعيد للدكتور
+	for (int i = 0; i < appointmentsCount; i++) {
+		if (appointments[i].doctorID == doctorID &&
+			appointments[i].appointmentDate.day == date.day &&
+			appointments[i].appointmentDate.month == date.month &&
+			appointments[i].appointmentDate.year == date.year &&
+			appointments[i].appointmentTime.hours == time.hours &&
+			appointments[i].appointmentTime.minutes == time.minutes &&
+			appointments[i].appointmentTime.timePeriod == time.timePeriod &&
+			appointments[i].status == "Booked")
+		{
+			cout << "Doctor already has an appointment at this time.\n";
+			return;
+		}
+	}
+
+	// تضارب مواعيد للمريض
+	for (int i = 0; i < appointmentsCount; i++) {
+		if (appointments[i].patientID == currentPatientID &&
+			appointments[i].appointmentDate.day == date.day &&
+			appointments[i].appointmentDate.month == date.month &&
+			appointments[i].appointmentDate.year == date.year &&
+			appointments[i].appointmentTime.hours == time.hours &&
+			appointments[i].appointmentTime.minutes == time.minutes &&
+			appointments[i].appointmentTime.timePeriod == time.timePeriod &&
+			appointments[i].status == "Booked")
+		{
+			cout << "You already booked an appointment at this time.\n";
+			return;
+		}
+	}
+
+	// توليد ID unique
+	int appointmentID = rand();
+	bool exists = true;
+	while (exists) {
+		exists = false;
+		for (int i = 0; i < appointmentsCount; i++) {
+			if (appointments[i].appointmentID == appointmentID) {
+				exists = true;
+				appointmentID = rand();
+				break;
+			}
+		}
+	}
+
+	// إضافة الموعد
+	appointments[appointmentsCount].appointmentID = appointmentID;
+	appointments[appointmentsCount].patientID = currentPatientID;
+	appointments[appointmentsCount].doctorID = doctorID;
+	appointments[appointmentsCount].appointmentDate = date;
+	appointments[appointmentsCount].appointmentTime = time;
+	appointments[appointmentsCount].status = "Booked";
+	appointments[appointmentsCount].totalCost = doctors[doctorIndex].consultationFee;
+
+	appointmentsCount++;
+
+	// تقليل Slots للدكتور
+	doctors[doctorIndex].availableSlots--;
+
+	cout << "\nAppointment booked successfully!\n";
+	cout << "Your Appointment ID is: " << appointmentID << endl;
+
+	patientDashboard();
+}
 void cancelAppointment() {};
 void viewAppointments() {};
 void patientLogout() {};
