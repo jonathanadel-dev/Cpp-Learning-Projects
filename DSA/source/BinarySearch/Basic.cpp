0163772742a1506bc7f5bb139a0036515fc14ff5
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2026-08-30 15:26:21 +0300

    BS | Iterative and recursive approaches

---
+++ DSA/source/BinarySearch/Basic.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <list>
+#include <set>
+#include <functional>
+#include <algorithm>
+using namespace std;
+
+int binarySearch(vector<int>& nums, int target) {
+
+	auto iterativeApproach = [&]() {
+		
+		int n = nums.size();
+		int index = -1, low = 0, high = n - 1;
+
+		while (low <= high) {
+
+			int mid = (low + high) / 2;
+
+			if (nums[mid] == target) {
+				return index = mid;
+				break;
+			}
+			else if (nums[mid] > target) {
+				high = mid - 1;
+			}
+			else {
+				low = mid + 1;
+			}
+
+		}
+
+		return index;
+
+	};
+
+	auto recursiveApproach = [&]() {
+	
+		int n = nums.size();
+
+		function<int(int, int)> search = [&](int low, int high) -> int {
+
+			if (low > high) {
+				return -1;
+			}
+
+			int mid = (low + high) / 2;
+			if (nums[mid] == target) {
+				return mid;
+			}
+			else if (nums[mid] > target) {
+				return search(low, mid - 1);
+			}
+			else {
+				return search(mid + 1, high);
+			}
+
+			return -1;
+
+		};
+
+		return search(0, n - 1);
+
+	};
+
+}
